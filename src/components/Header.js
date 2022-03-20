393998f8509191f70266064815b416a01cd19de9
RajaeAO <shikazukako1393@gmail.com>
2022-03-20 17:08:26 +0100

    add a paragraph

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Header = () => {
-  return <h2>Expense Tracker</h2>;
+  return (
+    <>
+      <h2>title</h2>
+      <p>paragraph</p>
+    </>
+  );
 };
-
 export default Header;
